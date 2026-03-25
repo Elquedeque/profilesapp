0d3d1f908c2dd1dfd81db0ba7feae4548ac30e98
Elquedeque <fasf040412@gmail.com>
2026-03-24 20:33:15 -0700

    Sincronizando dependencias para el build

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,72 @@
-import { useEffect, useState } from 'react'
-
-function App() {
-  const [documentos, setDocumentos] = useState([]);
-
-  // Esto se ejecuta cuando carga la página
+import { useState, useEffect } from "react";
+import {
+  Button,
+  Heading,
+  Flex,
+  View,
+  Grid,
+  Divider,
+} from "@aws-amplify/ui-react";
+import { useAuthenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+import { generateClient } from "aws-amplify/data";
+import outputs from "../amplify_outputs.json";
+/**
+ * @type {import('aws-amplify/data').Client<import('../amplify/data/
+resource').Schema>}
+ */
+Amplify.configure(outputs);
+const client = generateClient({
+  authMode: "userPool",
+});
+export default function App() {
+  const [userprofiles, setUserProfiles] = useState([]);
+  const { signOut } = useAuthenticator((context) => [context.user]);
   useEffect(() => {
-    // Aquí llamarás a tu API Gateway de AWS
-    fetch('')
-      .then(response => response.json())
-      .then(data => setDocumentos(data)) // 'data' sería la lista desde DynamoDB
+    fetchUserProfile();
   }, []);
-
+  async function fetchUserProfile() {
+    const { data: profiles } = await client.models.UserProfile.list();
+    setUserProfiles(profiles);
+  }
   return (
-    <div>
-      <h1>Mis Documentos de Clase</h1>
-      <ul>
-        {documentos.map((doc, index) => (
-          <li key={index}>
-            <a href={doc.url} target="_blank">{doc.nombre}</a>
-          </li>
+    <Flex
+      className="App"
+      justifyContent="center"
+      alignItems="center"
+      direction="column"
+      width="70%"
+      margin="0 auto"
+    >
+      <Heading level={1}>My Profile</Heading>
+      <Divider />
+      <Grid
+        margin="3rem 0"
+        autoFlow="column"
+        justifyContent="center"
+        gap="2rem"
+        alignContent="center"
+      >
+        {userprofiles.map((userprofile) => (
+          <Flex
+            key={userprofile.id || userprofile.email}
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            gap="2rem"
+            border="1px solid #ccc"
+            padding="2rem"
+            borderRadius="5%"
+            className="box"
+          >
+            <View>
+              <Heading level="3">{userprofile.email}</Heading>
+            </View>
+          </Flex>
         ))}
-      </ul>
-    </div>
+      </Grid>
+      <Button onClick={signOut}>Sign Out</Button>
+    </Flex>
   );
 }
-
-export default App
